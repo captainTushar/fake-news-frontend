2119e5269070f5bd3066d853b11963d8c7ad9908
captainTushar <tusharkandpal792@gmail.com>
2025-08-30 01:06:12 +0530

    Update backend URL for deployed API

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const handlePredict = async () => {
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://fake-news-backend-1-nzoc.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
